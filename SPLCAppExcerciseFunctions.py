94c8d9b6f01fe668a8cc4197583acce5017d6207
Kingsley Bawuah <kingsley404@gmail.com>
2018-06-25 00:12:53 -0400

    First commit

---
+++ SPLCAppExcerciseFunctions.py
@@ -0,0 +1,120 @@
+from urllib.request import urlopen as uRequest  # Importing HTML client
+from bs4 import BeautifulSoup as parser  # Importing Parsing Library.
+import nltk  # Importing Natural Language Toolkit
+from nltk.tokenize import RegexpTokenizer  # Importing tokenizer
+from collections import Counter  # Importing Counter library.
+
+
+
+# Function Name : wordCountDisplay
+# Function Parameters : List
+# Function Description : This function displays the number of elements in a list
+# Function Returns: Nothing.
+def wordCountDisplay(tokenList):
+    print("The number of words for this article: ")
+    print(len(tokenList))
+
+
+# Function Name : scrapensave
+# Function Parameters : myUrl,num
+# Function Description : This function connects, scrapes, and saves a webpage.
+# Function Returns: Returns the scraped data in lowercase format.
+def scrapensave(myUrl, num, num2=0):
+    # Open connection to the web-page, read, and close.
+    uClient = uRequest(myUrl)
+    pageHtml = uClient.read()
+    uClient.close()
+
+    # HTML "page soup" is stored here.
+    page_soup = parser(pageHtml, "html.parser")
+    if num2:
+        text = page_soup.find("div",
+                              {
+                                  "class": "field field-name-body field-type-text-with-summary field-label-above"}).getText()[
+               num:num2]
+    else:
+        text = page_soup.find("div",
+                              {
+                                  "class": "field field-name-body field-type-text-with-summary field-label-above"}).getText()[
+               num:]
+
+    # Return text in lowered form for uniformity when doing calculations.
+    return text.lower()
+
+
+# Function Name : removeStopWords
+# Function Parameters : List of tokens from text.
+# Function Description : This function will remove the english stop words from a list of tokens.
+# Function Returns: The list without the stop words included.
+def removeStopWords(tokenList):
+    eStopWords = nltk.corpus.stopwords.words('english')
+    newTokenList = []
+    for token in tokenList:
+        if token not in eStopWords:
+            newTokenList.append(token)
+    return newTokenList
+
+
+# Function Name : tokenizerList
+# Function Parameters : String
+# Function Description :
+# Function Returns:
+def tokenizerList(string):
+    # Using the regex for all words, NOTE/ISSUE: this will cause hyphenated words to be seperated and the hyphen ignored.
+    # Wasn't sure on the specifications for this.
+    tokenizer = RegexpTokenizer("([A-Za-z0-9-']+)")
+
+    tokens = tokenizer.tokenize(string)
+
+    return tokens
+
+
+# Function Name :
+# Function Parameters :
+# Function Description :
+# Function Returns :
+def plotFrequencies(listofTokens, title):
+    listofTokens = removeStopWords(listofTokens)
+    frequencyDist = nltk.FreqDist(listofTokens)
+    frequencyDist.plot(10, title=title, color="green",
+                       linewidth='10')
+
+
+# Function Name : displayCommon3
+# Function Parameters : List of tokenized text
+# Function Description : This function prints the top 3 most common words in a profile.
+# Function Returns : Nothing
+def displayCommon3(listofTokens):
+    listofTokens = removeStopWords(listofTokens)
+    frequencyDist = nltk.FreqDist(listofTokens)
+    frequencyDist = dict(Counter(frequencyDist).most_common(3))
+    # Now for each value in the sorted dictionary lets print.
+    print("The three most common values used in this Extremist profile are: ")
+    for word, count in frequencyDist.items():
+        print(word)
+
+
+# Function Name : displayCommon3
+# Function Parameters : List of tokenized text
+# Function Description : This function prints the top 3 most common words in a profile.
+# Function Returns : Nothing
+def displayshared3(listofTokens, listofTokens2):
+    listofTokens = removeStopWords(listofTokens)
+    listofTokens2 = removeStopWords(listofTokens2)
+
+    frequencyDist = nltk.FreqDist(listofTokens)
+    frequencyDist2 = nltk.FreqDist(listofTokens2)
+
+    freqIntersect = {}
+
+    allKeys = set(list(frequencyDist.keys()) + list(frequencyDist2.keys()))
+
+    for key in allKeys:
+        if key in frequencyDist and key in frequencyDist2:
+            freqIntersect[key] = frequencyDist[key] + frequencyDist2[key]
+
+    freqIntersect = dict(Counter(freqIntersect).most_common(3))
+
+    print("The three most common values used in both Extremist profiles are: ")
+    for word, count in freqIntersect.items():
+        print(word)
---
+++ SplcAppExercise.py
@@ -0,0 +1,51 @@
+# Title: Data Exercise for SPLC Canidates.
+# Description: Scrapes text from two extremist profiles, counts total words, and plots the frequency of words mentioned.
+# along with some calculation done in NLTK
+# Written by: Kingsley Bawuah
+# Date: 06/24/18
+from SPLCAppExcerciseFunctions import scrapensave
+from SPLCAppExcerciseFunctions import tokenizerList
+from SPLCAppExcerciseFunctions import wordCountDisplay
+from SPLCAppExcerciseFunctions import plotFrequencies
+from SPLCAppExcerciseFunctions import displayCommon3
+from SPLCAppExcerciseFunctions import displayshared3
+import nltk
+
+
+myUrl_1 = 'https://www.splcenter.org/fighting-hate/extremist-files/individual/mike-cernovich'
+myUrl_2 = 'https://www.splcenter.org/fighting-hate/extremist-files/individual/andrew-anglin'
+nltk.download('stopwords')
+
+# Main
+# First Profile + Graph
+print("-----------------------MIKE CERNOVICH -- PROFILE INFORMATION------------------------ ")
+data1 = scrapensave(myUrl_1, 11)
+listofTokens = tokenizerList(data1)
+wordCountDisplay(listofTokens)
+plotFrequencies(listofTokens, "Word Frequencies in Mike Cernovich's Extremist Profile")
+displayCommon3(listofTokens)
+
+# Second Profile + Graph
+print("--------------ANDREW ANGLIN -- PROFILE INFORMATION------------------- ")
+data2 = scrapensave(myUrl_2, 10)
+listofTokens2 = tokenizerList(data2)
+wordCountDisplay(listofTokens2)
+plotFrequencies(listofTokens2, "Word Frequencies in Andrew Anglin's Extremist Profile")
+displayCommon3(listofTokens2)
+
+# Most common values between both Profiles.
+print("----------------SHARED PROFILE INFORMATION-------------")
+displayshared3(listofTokens, listofTokens2)
+
+# Most common words only in the section "In their own words"
+# First Profile
+print("----------------MIKE CERNOVICH -- PROFILE INFORMATION -- IN THEIR OWN WORDS SECTION--------------- ")
+data1 = scrapensave(myUrl_1, 474, 2704)
+listofTokens = tokenizerList(data1)
+displayCommon3(listofTokens)
+
+#Second Profile
+print(" --------------ANDREW ANGLIN -- PROFILE INFORMATION-- IN THEIR OWN WORDS SECTION------------------ ")
+data2 = scrapensave(myUrl_2, 264, 2360)
+listofTokens2 = tokenizerList(data2)
+displayCommon3(listofTokens2)
